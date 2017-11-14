eb9a710d5bac71f066df28704bcd47975797cd8c
anant1297 <32320568+anant1297@users.noreply.github.com>
2017-11-14 20:50:42 +0530

    Update mil.py

--- mil.py
+++ mil.py
@@ -27,21 +27,8 @@ except sr.RequestError as e:
 import httplib, urllib
 import json
 
-# **********************************************
-# *** Update or verify the following values. ***
-# **********************************************
-
-# Replace the accessKey string value with your valid access key.
 accessKey = '70497ccef8024ce888ca11aa6d56df0b'
 
-# Replace or verify the region.
-#
-# You must use the same region in your REST API call as you used to obtain your access keys.
-# For example, if you obtained your access keys from the westus region, replace 
-# "westcentralus" in the URI below with "westus".
-#
-# NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
-# a free trial access key, you should not need to change this region.
 uri = 'westcentralus.api.cognitive.microsoft.com'
 path = '/text/analytics/v2.0/sentiment'
 
@@ -72,7 +59,7 @@ print (json.dumps(json.loads(result), indent=4))
 
 
 
-# Import the required module for text 
+# Importing the required module for text 
 # to speech conversion
 from gtts import gTTS
  
